ccb2906baa5ab27d076b034d38b882fe56533887
Chris Yu <chris_yu@epam.com>
2016-03-09 18:35:49 +0800

    update api

--- routes/api.js
+++ routes/api.js
@@ -1,57 +1,32 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
 var _ = require('underscore');
 
-var Article = mongoose.model('Article');
-
-//------------------------ use data from local json file
 // note that typically data would NOT be loaded from the filesystem in this manner :)
 
-// router.get('/articles', function(req, res, next) {
-// 	var fs = require('fs');
-// 	var obj;
-// 	fs.readFile('./data/articles.json', 'utf8', function (err, data) {
-// 	  if (err) throw err;
-// 	  res.json(JSON.parse(data));
-// 	});
-// });
-//------------------------ use data from mongo database
 router.get('/articles', function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Header", "X-Requestd-With");
-	Article.find({}, null, {sort: {data: -1}}, function(err, data) {
 
-		res.send(data);
+	var fs = require('fs');
+	var obj;
+	fs.readFile('./data/articles.json', 'utf8', function (err, data) {
+	  if (err) throw err;
+	  res.json(JSON.parse(data));
 	});
 });
 
 router.get('/articles/:id', function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Header", "X-Requestd-With");
-	Article.findById(req.params.id, function(err, data) {
-		console.log(data);
-		if(!err){
-			res.json(data);
-		} else {
-			res.send(404, "page not found");
-		}
+
+	var fs = require('fs');
+	var obj;
+	fs.readFile('./data/articles.json', 'utf8', function (err, data) {
+		if (err) throw err;
+
+		data = _.filter(JSON.parse(data), function(item) {
+		    return item.id == req.params.id;
+		});
+
+		res.json(data);
 	});
 });
-//------------------------ use data from local json file
-// router.get('/articles/:id', function(req, res, next) {
-// 	var fs = require('fs');
-// 	var obj;
-// 	fs.readFile('./data/articles.json', 'utf8', function (err, data) {
-// 		if (err) throw err;
-
-// 		data = _.filter(JSON.parse(data), function(item) {
-// 		    return item.id == req.params.id;
-// 		});
-
-// 		res.json(data);
-// 	});
-// });
-//---------------------------------
 
 module.exports = router;
--- routes/api_for_category.js
+++ routes/api_for_category.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var _ = require('underscore');
-
+// this Article collection actually is category
 var Article = mongoose.model('Article');
 
 router.get('/categories', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Header", "X-Requestd-With");
   Article.find({}, null, {sort: {data: -1}}, function(err, data) {
-
     res.send(data);
   });
 });
@@ -18,7 +16,6 @@ router.get('/categories/:id', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Header", "X-Requestd-With");
   Article.findById(req.params.id, function(err, data) {
-    console.log(data);
     if(!err){
       res.json(data);
     } else {
